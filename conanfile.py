65428ae76c536d4153865cd95d75c2681852bdaa
Matt Godbolt <matt@godbolt.org>
2018-08-18 15:17:48 -0500

    Initial import

---
+++ conanfile.py
@@ -0,0 +1,29 @@
+from conans import ConanFile, tools
+
+class CcsConan(ConanFile):
+    name = "ccs"
+    version = "0.9.13"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "compiler_args"
+    default_options="boost:shared=True"
+    scm = {
+            "type": "git",
+            "subfolder": "ccs",
+            "url": "https://github.com/hellige/ccs-cpp.git",
+            "revision" : version
+            }
+    requires = "boost/1.67.0@conan/stable"
+    build_requires = "gtest/1.8.0@bincrafters/stable"
+
+    def source(self):
+        tools.patch(base_path='ccs', patch_file='conanbuild.patch')
+
+    def package(self):
+        self.copy("*", src='ccs/dist')
+
+    def package_info(self):
+        self.cpp_info.libs = self.collect_libs()
+
+    def build(self):
+        conanflags=open('conanbuildinfo.args').read()
+        self.run("make CONANFLAGS='{}' -j{}".format(conanflags, tools.cpu_count()), cwd='ccs')
